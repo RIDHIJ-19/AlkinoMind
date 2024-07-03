6c3c3451b0b8c3a6668a8322a7660fa771f7011c
RIDHIJ-19 <128258454+RIDHIJ-19@users.noreply.github.com>
2024-07-04 02:13:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from akinator_python import Akinator
+
+akinator = Akinator(lang="en")
+akinator.start_game()
+while True:
+    try:
+        print(akinator.question)
+        ans = input("answer：")
+        if ans == "b":
+            akinator.go_back()
+        else:
+            akinator.post_answer(ans)
+            if akinator.answer_id:
+                print(f"{akinator.name} / {akinator.description}")
+                ans = input("is it correct?：")
+                if ans == "n":
+                    akinator.exclude()
+                elif ans == "y":
+                    break
+                else:
+                    break
+    except Exception as e:
+        print(e)
+        continue
